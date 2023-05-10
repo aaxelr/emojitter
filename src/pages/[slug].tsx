ad9718a6613d415e83db823c38b43b955cf8aa55
aaxelr <antonio.axelsson@gmail.com>
2023-05-10 17:36:06 +0200

    Show profile pic on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,8 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -20,9 +22,18 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{data.username} | Profile</div>
-      </main>
+      <PageLayout>
+        <div className="bg-gradient-to-b from-slate-400 to-black  p-8">
+          <Image
+            className="rounded-full"
+            src={data.profileImageUrl}
+            alt={`Profile picture for user ${data.username ?? ""}.`}
+            width={128}
+            height={128}
+          />
+          <div className="mt-6 text-3xl font-bold">{`@${data.username}`}</div>
+        </div>
+      </PageLayout>
     </>
   );
 };
