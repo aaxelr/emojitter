bae5097c1d0a6608a22b94226202a7510ed566a6
aaxelr <antonio.axelsson@gmail.com>
2023-05-11 14:45:07 +0200

    Add SinglePostPage

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,17 +1,55 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+import { generateSShelper } from "~/server/helpers/ssHelper";
 
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) {
+    return (
+      <div className="flex h-screen items-center justify-center text-2xl">
+        Something went wrong ¯\_(ツ)_/¯
+      </div>
+    );
+  }
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Single Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = generateSShelper();
+
+  const id = context.params?.id;
+
+  // TODO handle error better. return to different page?
+  if (typeof id !== "string") throw new Error("no slug");
+
+  await helpers.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default SinglePostPage;
