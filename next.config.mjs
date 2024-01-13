5589c4ea17a6852d58273395662756c67377fe75
aaxelr <antonio.axelsson@gmail.com>
2024-01-13 18:09:05 +0100

    update images domains

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["images.clerk.dev"],
+    domains: ["img.clerk.com"],
   },
   typescript: {
     ignoreBuildErrors: true,
