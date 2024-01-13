5732ccb716031a6d3d3f69201bd329d7d595c3e2
aaxelr <antonio.axelsson@gmail.com>
2024-01-13 12:39:04 +0100

    fix type import

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,8 +1,8 @@
-import type { User } from "@clerk/nextjs/dist/api";
+import type { User } from "@clerk/nextjs/dist/types/server";
 export const filterUserForClent = (user: User) => {
   return {
     id: user.id,
     username: user.username,
-    profileImageUrl: user.profileImageUrl,
+    profileImageUrl: user.imageUrl,
   };
 };
